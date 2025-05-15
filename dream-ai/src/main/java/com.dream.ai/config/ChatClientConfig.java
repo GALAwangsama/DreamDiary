0d6a240fee4702b6da000a1f7dcd490b06011da4
GALAwang <3221134362@qq.com>
2025-05-15 20:00:34 +0800

    feat: 新增dream-ai模块，基于deepseek的模型对话

---
+++ dream-ai/src/main/java/com.dream.ai/config/ChatClientConfig.java
@@ -0,0 +1,44 @@
+package com.dream.ai.config;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+
+@Configuration
+public class ChatClientConfig {
+
+    //系统提示词
+    private static final String DEFAULT_PROMPT = "你现在是一位管理智能开发模板的智能体，你需要根据用户的请求做出相应的回答，帮助用户完成任务";
+
+    @Resource
+    private OpenAiChatModel DeepSeekChatModel;
+
+    //选择模型创建ChatClient
+    @Bean(name ="DeepSeekChatClient")
+    public ChatClient ChatClient(){
+        return ChatClient.builder(DeepSeekChatModel)
+                .defaultSystem(DEFAULT_PROMPT)
+                // 实现 Chat Memory 的 Advisor
+                // 在使用 Chat Memory 时，需要指定对话 ID，以便 Spring AI 处理上下文。
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(new InMemoryChatMemory())
+                )
+                //实现 Logger 的 Advisor
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor()
+                )
+                .defaultOptions(OpenAiChatOptions.builder().temperature(0.7d).build())
+                .build();
+    }
+
+
+}
